40b6f5b6073451489475dcbbae4f75f678d158fd
Ezequiel Santalla <ezequielasantalla@gmail.com>
2024-09-22 21:52:34 -0300

    adding SSR to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-// @ts-check
+// @ts-nocheck
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import vercelServerless from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
+  adapter: vercelServerless()
 });
